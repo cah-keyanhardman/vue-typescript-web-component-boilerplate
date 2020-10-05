12bf935d1d8741958f409135afb6004a75662e9b
cah-keyanhardman <keyanhardman@gmail.com>
2020-10-05 10:31:42 -0400

    [CPCS-38] Build/release pipeline

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import ConnectPlusPatientInfo from '@/ConnectPlusPatientInfo.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('ConnectPlusPatientInfo.vue', () => {
+  it('placeholder', () => {
+    const wrapper = shallowMount(ConnectPlusPatientInfo, {})
+    expect(true).toEqual(true)
   })
 })
